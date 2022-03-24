03468bd2327d87dee8f45e1cca7f69c03e3cf5ff
Isabela <duartemorenoisabela@gmail.com>
2022-03-24 00:26:04 -0300

    Ajustando DataConfiguration e adicionando dependências

--- src/main/java/com/customanagerapi/DataConfiguration.java
+++ src/main/java/com/customanagerapi/DataConfiguration.java
@@ -3,7 +3,7 @@ package com.customanagerapi;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-//import org.apache.tomcat.jdbc.pool.DataSource;
+import org.apache.tomcat.jdbc.pool.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -12,19 +12,19 @@ import org.springframework.context.annotation.Profile;
 @Profile("prod")
 public class DataConfiguration {
 
-//	@Bean
-//    public DataSource dataSource() throws URISyntaxException {
-//        URI dbUri = new URI(System.getenv("DATABASE_URL"));
-//
-//        String username = dbUri.getUserInfo().split(":")[0];
-//        String password = dbUri.getUserInfo().split(":")[1];
-//        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
-//
-//        DataSource basicDataSource = new DataSource();
-//        basicDataSource.setUrl(dbUrl);
-//        basicDataSource.setUsername(username);
-//        basicDataSource.setPassword(password);
-//
-//        return basicDataSource;
-//    }
+	@Bean
+    public DataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+        DataSource basicDataSource = new DataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
+    }
 }
