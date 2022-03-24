80b7297bb6c53dcf4c174b5ded3c4c41747ce642
Isabela <duartemorenoisabela@gmail.com>
2022-03-24 01:00:53 -0300

    Ajuste procfile annotation

--- src/main/java/com/customanagerapi/DataConfiguration.java
+++ src/main/java/com/customanagerapi/DataConfiguration.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("prod")
+@Profile("Procfile")
 public class DataConfiguration {
 
 	@Bean
